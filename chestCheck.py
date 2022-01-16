2575d37631fafe66985da3aa52d2b948ff838c57
Bluscream <Bluscream@users.noreply.github.com>
2022-01-16 23:39:00 +0100

    made chestcheck more useful

--- chestCheck.py
+++ chestCheck.py
@@ -8,7 +8,23 @@ px = int(Player.getPlayer().getX())
 py = int(Player.getPlayer().getY())
 pz = int(Player.getPlayer().getZ())
 
-container = ["minecraft:chest"]
+chests = ["minecraft:chest", "minecraft:trapped_chest"]
+container = chests + [
+    "minecraft:ender_chest",
+    "minecraft:barrel",
+    "minecraft:shulker_box",
+    "minecraft:dispenser",
+    "minecraft:dropper",
+    "minecraft:hopper",
+    "minecraft:furnace",
+    "minecraft:lit_furnace",
+    "minecraft:blast_furnace",
+    "minecraft:smoker",
+    "minecraft:lit_smoker",
+    "minecraft:campfire",
+    "minecraft:lit_campfire",
+    "minecraft:chest_minecart",
+]
 
 container2 = [
     "AbstractBannerBlock",
@@ -47,17 +63,20 @@ for x in range(px - radius, px + radius):
             
             block = World.getBlock(x, y, z)
             # if isinstance(block, ChestBlock.getClass()):
-            if block != None and block.getId() in container:
-                blockState = block.getRawBlockState()
-                if blockState.method_11654(ChestBlock.field_10770) in sides:
-                    continue
-                Player.getPlayer().lookAt(x + 0.5, y + 0.5, z + 0.5)
-                Client.waitTick(2)
-                
-                if Player.rayTraceBlock(4, False) != None and Player.rayTraceBlock(4, False).getId() in container:
-                    Chat.log("interact")
-                    Player.getPlayer().interact()
-                    Client.waitTick(7)
-                    Player.openInventory().close()
-                    #Player.getPlayer().interactBlock(x, y, z, 0, False)
-                    #Client.waitTick(2)
+            if block != None:
+                block_id = block.getId()
+                if block_id in container:
+                    if block_id in chests:
+                        blockState = block.getRawBlockState()
+                        if blockState.method_11654(ChestBlock.field_10770) in sides:
+                            continue
+                    Player.getPlayer().lookAt(x + .5, y + .5, z + .5)
+                    Client.waitTick(2)
+                    
+                    if Player.rayTraceBlock(4, False) != None and Player.rayTraceBlock(4, False).getId() in container:
+                        Chat.log(f"Interacting with {block.getName()} at [{x}, {y}, {z}]")
+                        Player.getPlayer().interact()
+                        Client.waitTick(8)
+                        Player.openInventory().close()
+                        #Player.getPlayer().interactBlock(x, y, z, 0, False)
+                        #Client.waitTick(2)
